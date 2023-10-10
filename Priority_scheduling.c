387b2066245db9f6816ca7935fcbbe44f5b29ce5
shovik banerjee <143200380+Shovik-2004@users.noreply.github.com>
2023-10-10 08:56:42 +0530

    Update Priority_scheduling.c

--- Priority_scheduling.c
+++ Priority_scheduling.c
@@ -1,102 +1,92 @@
-#include<stdio.h>  
-#include<stdlib.h>  
-  
-struct process {  
-    int process_id;  
-    int burst_time;  
-    int priority;  
-    int waiting_time;  
-    int turnaround_time;  
-};  
-  
-void find_waiting_time(struct process[], int, int[]);  
-void find_turnaround_time(struct process[], int, int[], int[]);  
-  
-void find_average_time(struct process[], int);  
-  
-void priority_scheduling(struct process[], int);  
-  
-int main()  
-{  
-    int n, i;  
-    struct process proc[10];  
-  
-printf("Enter the number of processes: ");  
-scanf("%d", &n);  
-  
-for(i = 0; i< n; i++)  
-    {  
-printf("\nEnter the process ID: ");  
-scanf("%d", &proc[i].process_id);  
-  
-printf("Enter the burst time: ");  
-scanf("%d", &proc[i].burst_time);  
-  
-printf("Enter the priority: ");  
-scanf("%d", &proc[i].priority);  
-    }  
-  
-priority_scheduling(proc, n);  
-    return 0;  
-}  
-  
-void find_waiting_time(struct process proc[], int n, int wt[])  
-{  
-    int i;  
-wt[0] = 0;  
-  
-for(i = 1; i< n; i++)  
-    {  
-wt[i] = proc[i - 1].burst_time + wt[i - 1];  
-    }  
-}  
-  
-void find_turnaround_time(struct process proc[], int n, int wt[], int tat[])  
-{  
-    int i;  
-for(i = 0; i< n; i++)  
-    {  
-tat[i] = proc[i].burst_time + wt[i];  
-    }  
-}  
-  
-void find_average_time(struct process proc[], int n)  
-{  
-    int wt[10], tat[10], total_wt = 0, total_tat = 0, i;  
-  
-find_waiting_time(proc, n, wt);  
-find_turnaround_time(proc, n, wt, tat);  
-  
-printf("\nProcess ID\tBurst Time\tPriority\tWaiting Time\tTurnaround Time");  
-  
-for(i = 0; i< n; i++)  
-    {  
-total_wt = total_wt + wt[i];  
-total_tat = total_tat + tat[i];  
-printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", proc[i].process_id, proc[i].burst_time, proc[i].priority, wt[i], tat[i]);  
-    }  
-  
-printf("\n\nAverage Waiting Time = %f", (float)total_wtotal_wt/n);  
-printf("\nAverage Turnaround Time = %f\n", (float)total_tat/n);  
-}  
-  
-void priority_scheduling(struct process proc[], int n)  
-{  
-int i, j, pos;  
-struct process temp;  
-for(i = 0; i< n; i++)  
-{  
-    pos = i;  
-for(j = i + 1; j < n; j++)  
-    {  
-        if(proc[j].priority< proc[pos].priority)  
-            pos = j;  
-    }  
-  
-    temp = proc[i];  
-    proc[i] = proc[pos];  
-    proc[pos] = temp;  
-}  
-  
-find_average_time(proc, n);  
-}  
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct process {
+    int process_id;
+    int burst_time;
+    int priority;
+    int waiting_time;
+    int turnaround_time;
+};
+
+void find_waiting_time(struct process[], int, int[]);
+void find_turnaround_time(struct process[], int, int[], int[]);
+void find_average_time(struct process[], int);
+void priority_scheduling(struct process[], int);
+
+int main() {
+    int n, i;
+    struct process proc[10];
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("\nEnter the process ID: ");
+        scanf("%d", &proc[i].process_id);
+
+        printf("Enter the burst time: ");
+        scanf("%d", &proc[i].burst_time);
+
+        printf("Enter the priority: ");
+        scanf("%d", &proc[i].priority);
+    }
+
+    priority_scheduling(proc, n);
+    return 0;
+}
+
+void find_waiting_time(struct process proc[], int n, int wt[]) {
+    // Initialize the waiting time for the first process
+    wt[0] = 0;
+
+    // Calculate waiting time for each process
+    for (int i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + proc[i - 1].burst_time;
+    }
+}
+
+void find_turnaround_time(struct process proc[], int n, int wt[], int tat[]) {
+    // Calculate turnaround time for each process
+    for (int i = 0; i < n; i++) {
+        tat[i] = wt[i] + proc[i].burst_time;
+    }
+}
+
+void find_average_time(struct process proc[], int n) {
+    int wt[10], tat[10];
+    float total_wt = 0, total_tat = 0;
+
+    find_waiting_time(proc, n, wt);
+    find_turnaround_time(proc, n, wt, tat);
+
+    printf("\nProcess ID\tBurst Time\tPriority\tWaiting Time\tTurnaround Time");
+
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", proc[i].process_id, proc[i].burst_time, proc[i].priority, wt[i], tat[i]);
+    }
+
+    // Calculate and print the average waiting time and average turnaround time
+    printf("\n\nAverage Waiting Time = %.2f", total_wt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);
+}
+
+void priority_scheduling(struct process proc[], int n) {
+    // Sort the processes based on priority (ascending order)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (proc[j].priority > proc[j + 1].priority) {
+                // Swap the processes
+                struct process temp = proc[j];
+                proc[j] = proc[j + 1];
+                proc[j + 1] = temp;
+            }
+        }
+    }
+
+    // Calculate and display the average waiting time and average turnaround time
+    find_average_time(proc, n);
+}
